713d0b9dd4a1c5810da9dc9fa66d45630e346a86
180312 <31240925+180312@users.noreply.github.com>
2019-02-12 15:40:53 +0100

    Updates

---
+++ precache-manifest.e240aee5a4330072fc55f860c047fc72.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec230cebd5b0d802f3bd",
+    "url": "/terningspillet/static/js/main.ec230ceb.chunk.js"
+  },
+  {
+    "revision": "cd7bfd4816f4f40d8ac9",
+    "url": "/terningspillet/static/js/runtime~main.cd7bfd48.js"
+  },
+  {
+    "revision": "f0118d36c7749a46ed8d",
+    "url": "/terningspillet/static/js/2.f0118d36.chunk.js"
+  },
+  {
+    "revision": "ecd419137d1cdb33e9af9ba7094003d4",
+    "url": "/terningspillet/index.html"
+  }
+];
